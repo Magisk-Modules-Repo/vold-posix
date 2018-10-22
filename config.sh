d4c5b336700d10150f919e009929571c60707d50
noname8964 <32409462+noname8964@users.noreply.github.com>
2018-10-22 16:59:54 +0800

    update files permission

--- config.sh
+++ config.sh
@@ -86,6 +86,7 @@ set_permissions() {
   # The following is default permissions, DO NOT remove
   set_perm_recursive $MODPATH  0  0  0755  0644
   set_perm_recursive $MODPATH/system  0  2000  0755  0644
+  set_perm_recursive $MODPATH/system/bin  0  2000  0755  0755
   set_perm  $MODPATH/system/bin/vold  0  2000  0755  u:object_r:vold_exec:s0
   set_perm  $MODPATH/system/bin/fsck.exfat  0  2000  0755  u:object_r:fsck_exec:s0
   set_perm  $MODPATH/system/bin/fsck.ntfs  0  2000  0755  u:object_r:fsck_exec:s0
