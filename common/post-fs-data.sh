4a5ce061998944a54b0c8518c55e3e4eaf614bcf
noname8964 <32409462+noname8964@users.noreply.github.com>
2018-10-26 17:35:36 +0800

    Move add sepolicy to post-fs-data mode and update some sepolicy rules. Remove restart vold for "Android for Wrok" compatibility.

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,25 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+magiskpolicy --live "attradd priv_app mlstrustedsubject"
+magiskpolicy --live "attradd platform_app mlstrustedsubject"
+magiskpolicy --live "attradd mediaprovider mlstrustedsubject"
+magiskpolicy --live "attradd vfat mlstrustedobject"
+magiskpolicy --live "allow vfat vfat filesystem associate"
+magiskpolicy --live "allow vold labeledfs filesystem relabelfrom"
+magiskpolicy --live "allow vold unlabeled filesystem relabelfrom"
+magiskpolicy --live "allow vold vfat filesystem { mount unmount relabelto relabelfrom }"
+magiskpolicy --live "allow vold vfat dir { getattr setattr }"
+magiskpolicy --live "allow vold mnt_media_rw_stub_file dir { open read }"
+#magiskpolicy --live "allow fsck_untrusted vfat dir getattr"
+#magiskpolicy --live "allow fsck_untrusted vfat file getattr"
+#magiskpolicy --live "allow fsck_untrusted rootfs blk_file getattr"
+#magiskpolicy --live "allow fsck_untrusted block_device dir getattr"
+#magiskpolicy --live "allow fsck_untrusted system_file file entrypoint"
+#magiskpolicy --live "allow fsck_untrusted fsck_untrusted capability sys_admin"
+magiskpolicy --live "allow system_server vfat dir { read open getattr }"
+magiskpolicy --live "allow system_server vfat file { read open getattr }"
+magiskpolicy --live "allow { vold system_app untrusted_app_all } vfat filesystem getattr"
+magiskpolicy --live "allow { vold system_app untrusted_app_all } vfat dir { open getattr setattr read write search ioctl add_name remove_name create reparent rename rmdir }"
+magiskpolicy --live "allow { vold system_app untrusted_app_all } vfat file { open read write append ioctl lock create rename getattr setattr unlink }"
--- common/service.sh
+++ common/service.sh
@@ -6,25 +6,4 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-magiskpolicy --live "permissive priv_app"
-magiskpolicy --live "permissive platform_app"
-magiskpolicy --live "permissive mediaprovider"
-magiskpolicy --live "allow vfat vfat filesystem associate"
-magiskpolicy --live "allow vold labeledfs filesystem relabelfrom"
-magiskpolicy --live "allow vold unlabeled filesystem relabelfrom"
-magiskpolicy --live "allow vold vfat filesystem { mount unmount relabelto relabelfrom }"
-magiskpolicy --live "allow vold vfat dir { getattr setattr }"
-magiskpolicy --live "allow vold mnt_media_rw_stub_file dir { open read }"
-magiskpolicy --live "allow fsck_untrusted vfat dir getattr"
-magiskpolicy --live "allow fsck_untrusted vfat file getattr"
-magiskpolicy --live "allow fsck_untrusted rootfs blk_file getattr"
-magiskpolicy --live "allow fsck_untrusted block_device dir getattr"
-magiskpolicy --live "allow fsck_untrusted system_file file entrypoint"
-magiskpolicy --live "allow fsck_untrusted fsck_untrusted capability sys_admin"
-magiskpolicy --live "allow system_server vfat dir { read open getattr }"
-magiskpolicy --live "allow system_server vfat file { read open getattr }"
-magiskpolicy --live "allow { vold system_app untrusted_app_all } vfat filesystem getattr"
-magiskpolicy --live "allow { vold system_app untrusted_app_all } vfat dir { open getattr setattr read write search ioctl add_name remove_name create reparent rename rmdir }"
-magiskpolicy --live "allow { vold system_app untrusted_app_all } vfat file { open read write append ioctl lock create rename getattr setattr unlink }"
 
-stop vold && start vold
